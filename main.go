bd3387f4a43eec2990eeeb8a65a6dbc1c31f6545
刘新军 <liuxinjun@021.com>
2024-04-03 10:47:00 +0800

    go初始化

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "info",
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", ping)
+	r.Run()
+}
